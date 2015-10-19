c1a6ed451f7f9ff9f47287bcb0129817e346c759
Stephen Stack <sstack@punchthrough.com>
2015-10-19 13:13:11 -0500

    gui: make icons looks better in BeanList

--- app/frontend/components/Icon.jsx
+++ app/frontend/components/Icon.jsx
@@ -6,7 +6,6 @@ import React from 'react'
 
 const DEVICE_TYPE_BEAN = require('../../lightblue/devices').consts.DEVICE_TYPE_BEAN
 const DEVICE_TYPE_BLE = require('../../lightblue/devices').consts.DEVICE_TYPE_BLE
-let icon_classes = 'img-circle media-object pull-left'
 
 class Icon extends React.Component {
   constructor() {
@@ -14,31 +13,22 @@ class Icon extends React.Component {
   }
 
   render() {
+    let icon_classes = 'img-circle media-object pull-left'
 
-    return (
-      <div>
-        <p>
-          {(() => {
-            switch (this.props.device_type) {
-              case DEVICE_TYPE_BEAN:
-                icon_classes += ' icon-bean'
-                return (
-                  <img className={icon_classes} src="assets/images/bean.png" width="32" height="32">
-                  </img>
-                )
-              break
-              default:
-                icon_classes += ' icon-ble'
-                return (
-                  <img className={icon_classes} src="assets/images/ble.png" width="32" height="32">
-                  </img>
-                )
-              break
-            }
-          })()}
-        </p>
-      </div>
-    )
+    switch (this.props.device_type) {
+      case DEVICE_TYPE_BEAN:
+        icon_classes += ' icon-bean'
+        return (
+          <img className={icon_classes} src="assets/img/bean-full.png"></img>
+        )
+        break
+      default:
+        icon_classes += ' icon-ble'
+          return (
+            <img className={icon_classes} src="assets/img/ble-full.png"></img>
+          )
+        break
+    }
   }
 }
 

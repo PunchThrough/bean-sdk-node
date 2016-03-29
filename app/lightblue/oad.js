4afed27160d3fd95c352cf51d50d945b6257c3e8
Stephen Stack <ss@stephenstack.com>
2016-03-29 09:45:44 -0700

    oad: turbo oad implementation

--- app/lightblue/oad.js
+++ app/lightblue/oad.js
@@ -5,7 +5,7 @@ let fs = require('fs')
 let path = require('path')
 let buffer = require('buffer')
 
-const FW_VERSION = '20151027'
+const FW_VERSION = '20160328'
 const FW_FILES = path.join(__dirname, '..', 'resources', 'firmware_bundles', FW_VERSION)
 const BLOCK_LENGTH = 16
 const FW_HEADER_LENGTH = 12
@@ -22,6 +22,8 @@ const OAD_STEP_STATE_CHECKING_FW = 'OAD_STEP_STATE_CHECKING_FW'
 const OAD_STEP_STATE_OFFERING_FILES = 'OAD_STEP_STATE_OFFERING_FILES'
 const OAD_STEP_STATE_BLOCK_XFER = 'OAD_STEP_STATE_BLOCK_XFER'
 const OAD_STEP_STATE_REBOOTING = 'OAD_STEP_STATE_REBOOTING'
+
+const PKT_DIFF_PER_SENT_AND_REQUESTED = 8
 
 
 class FirmwareUpdater {
@@ -51,6 +53,7 @@ class FirmwareUpdater {
     this._step = null
     this._stateGlobal = OAD_STATE_NOT_IN_PROGRESS
     this._stateStep = null
+    this._nextBlockNoToSent = 0
   }
 
   _fail(err) {
@@ -85,50 +88,65 @@ class FirmwareUpdater {
      * @param buf 2 byte Buffer containing the block number
      */
 
-    let blkNo = buf.readUInt16LE(0, 2)
-    this._lastBlock = blkNo
+    let blkNoRequested = buf.readUInt16LE(0, 2)
 
-    if (blkNo % 200 === 0)
-      console.log(`Got request for FW block #${blkNo}`)
+    while (this._nextBlockNoToSent < (blkNoRequested + PKT_DIFF_PER_SENT_AND_REQUESTED) && this._stateStep != OAD_STEP_STATE_REBOOTING )
+    {
+      let blkDifference = PKT_DIFF_PER_SENT_AND_REQUESTED - (this._nextBlockNoToSent - blkNoRequested)
+      //console.log('_nextBlockNoToSent = %s, block requested = %s, block diff = %s',this._nextBlockNoToSent, blkNoRequested,  blkDifference )
+      let blkNo = this._nextBlockNoToSent
 
-    if (blkNo === 0) {
-      console.log('ACCEPTED IMAGE: %s', this._fwfiles[this._fileOfferedIndex])
-      console.log('Got request for the first BLOCK of FW')
-      this._stateStep = OAD_STEP_STATE_BLOCK_XFER
+      if (blkNo % 512 === 0)
+        console.log(`Got request for FW block #${blkNo} or %s`, blkNo*BLOCK_LENGTH)
 
-      // calculate size of image to get total blocks
-      let fwFileStats = fs.statSync(path.join(FW_FILES, this._fwfiles[this._fileOfferedIndex]))
-      this._totalBlocks = (fwFileStats.size / BLOCK_LENGTH) - 1
-      console.log(`Total blocks: ${this._totalBlocks}`)
-      console.log(`FW file size: ${fwFileStats.size}`)
-      this._step += 1
-      console.log(`Starting step #${this._step}!`)
-      this._lb.stopScanning()
-    }
+      if (blkNo === 0) {
+        console.log('ACCEPTED IMAGE: %s', this._fwfiles[this._fileOfferedIndex])
+        console.log('Got request for the first BLOCK of FW')
+        this._stateStep = OAD_STEP_STATE_BLOCK_XFER
 
-    // read block from open file
-    let fileOffset = blkNo * BLOCK_LENGTH
-    let blkBuf = new buffer.Buffer(BLOCK_LENGTH)
-    let bytesRead = fs.readSync(this._currentFwFile, blkBuf, 0, BLOCK_LENGTH, fileOffset)
-    if (bytesRead != BLOCK_LENGTH) {
-      return this._fail('Internal error: failed to read FW file')
-    }
+        // calculate size of image to get total blocks
+        let fwFileStats = fs.statSync(path.join(FW_FILES, this._fwfiles[this._fileOfferedIndex]))
+        this._totalBlocks = (fwFileStats.size / BLOCK_LENGTH) - 1
+        console.log(`Total blocks: ${this._totalBlocks}`)
+        console.log(`FW file size: ${fwFileStats.size}`)
+        this._step += 1
+        console.log(`Starting step #${this._step}!`)
+        this._lb.stopScanning()
+      }
+      else {
+          //console.log('FAILE--D IMAGE: %s', this._currentFwFile)
+      }
 
-    let finalBuf = buffer.Buffer.concat([buf, blkBuf])
-    this._deviceInProgress.getOADService().writeToBlock(finalBuf, (err)=> {
-      if (err)
-        console.log(`Error writing to block char: ${err}`)
-    })
+      // read block from open file
+      let fileOffset = blkNo * BLOCK_LENGTH
+      let blkBuf = new buffer.Buffer(BLOCK_LENGTH)
+      let bytesRead = fs.readSync(this._currentFwFile, blkBuf, 0, BLOCK_LENGTH, fileOffset)
+      if (bytesRead != BLOCK_LENGTH) {
+        return this._fail('Internal error: failed to read FW file...')
+      }
+      //Kianoosh: Begin
+      let blockAddr = new buffer.Buffer(2)
+      blockAddr[0] =  blkNo & 0xFF
+      blockAddr[1] = (blkNo >> 8) & 0xFF
+      let finalBuf = buffer.Buffer.concat([blockAddr, blkBuf])
+      this._deviceInProgress.getOADService().writeToBlock(finalBuf, (err)=> {
+        if (err)
+          console.log(`Error writing to block char: ${err}`)
+      })
 
-    if (blkNo === this._totalBlocks) {
-      console.log('Last block!')
+      if (blkNo === this._totalBlocks) {
+        console.log('Last block!')
+        this._nextBlockNoToSent = 0 // Reset Back to 0 for new file or done FWU!
+        // reset fileOfferedIndex
+        this._fileOfferedIndex = -1
 
-      // reset fileOfferedIndex
-      this._fileOfferedIndex = -1
-
-      this._lb.startScanning()
-      this._stateStep = OAD_STEP_STATE_REBOOTING
-      console.log(`Waiting for device to reset: ${this._deviceInProgress.toString()}`)
+        this._lb.startScanning()
+        this._stateStep = OAD_STEP_STATE_REBOOTING
+        console.log(`Waiting for device to reset: ${this._deviceInProgress.toString()}`)
+      }
+      else {
+        this._nextBlockNoToSent += 1
+      }
     }
   }
 
@@ -162,7 +180,18 @@ class FirmwareUpdater {
     }
 
     console.log(`Offering image: ${filename}`)
+    //Kianoosh: Begin
+    console.log(hdrBuf.toString('hex'))
+    console.log('Kianoosh: Header[0]: %s', hdrBuf[0])
+    console.log('Kianoosh: Header[1]: %x', hdrBuf[1])
+    console.log('Kianoosh: Header[2]: %x', hdrBuf[2])
+    console.log('Kianoosh: Header[3]: %x', hdrBuf[3])
+    console.log('Kianoosh: Header[4]: %s', hdrBuf[4])
+    console.log('Kianoosh: Header[5]: %x', hdrBuf[5])
+    console.log('Kianoosh: Header[6]: %x', hdrBuf[6])
+    console.log('Kianoosh: Header[7]: %x', hdrBuf[7])
 
+    //Kianoosh: End
     this._deviceInProgress.getOADService().writeToIdentify(hdrBuf, (err)=> {
       if (err)
         console.log(`Error writing to identify char: ${err}`)
@@ -187,7 +216,7 @@ class FirmwareUpdater {
         let v = ''
         if (buffer.Buffer.isBuffer(fwVersion))
           v = fwVersion.toString('utf8').split(' ')[0]
-        console.log(`Comparing firmware versions ${this._storedFwVersion} and ${v}`)
+        console.log(`Comparing firmware versions stored version ${this._storedFwVersion} and Read Version ${v}`)
         if (this._storedFwVersion === v && this._deviceInProgress != null) {
           callback('Versions are the same, no update needed')
         } else {

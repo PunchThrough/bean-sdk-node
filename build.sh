027c5202ae169db168bfa706dce6956c2c2c6961
Stephen Stack <sstack@punchthrough.com>
2016-08-31 16:55:35 -0500

    install: monkey patch platform.local.txt with our own

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-BEAN_ARDUINO_CORE="bean-arduino-core-2.0.0.tar.gz"
+BEAN_ARDUINO_CORE="bean-arduino-core-2.0.0"
+BEAN_ARDUINO_CORE_TARBALL="${BEAN_ARDUINO_CORE}.tar.gz"
 
 ./node_modules/.bin/babel src -d build/ &&
 cp -R src/resources/ build/resources/ &&
-tar -xzvf "build/resources/${BEAN_ARDUINO_CORE}" -C build/resources/ &&
-rm "build/resources/${BEAN_ARDUINO_CORE}" &&
+tar -xzvf "build/resources/${BEAN_ARDUINO_CORE_TARBALL}" -C build/resources/ &&
+cp src/resources/platform.local.txt "build/resources/${BEAN_ARDUINO_CORE}/hardware/LightBlue-Bean/bean/avr/"
+rm "build/resources/${BEAN_ARDUINO_CORE_TARBALL}" &&
 echo "Build Complete!"

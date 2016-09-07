6b505addbf632f8da138fa30aaac9f2906c07dd2
Stephen Stack <Stephen.Stack@etherios.com>
2016-09-07 13:16:30 -0500

    build: fix some order of operation issues in build script

--- build.sh
+++ build.sh
@@ -3,9 +3,12 @@
 BEAN_ARDUINO_CORE="bean-arduino-core-2.0.0"
 BEAN_ARDUINO_CORE_TARBALL="${BEAN_ARDUINO_CORE}.tar.gz"
 
+rm -r build/
+mkdir build/
 ./node_modules/.bin/babel src -d build/ &&
 cp -R src/resources/ build/resources/ &&
-tar -xzvf "build/resources/${BEAN_ARDUINO_CORE_TARBALL}" -C build/resources/ &&
-cp src/resources/platform.local.txt "build/resources/${BEAN_ARDUINO_CORE}/hardware/LightBlue-Bean/bean/avr/"
 rm "build/resources/${BEAN_ARDUINO_CORE_TARBALL}" &&
+tar -xzvf "src/resources/${BEAN_ARDUINO_CORE_TARBALL}" -C build/resources/ &&
+cp src/resources/platform.local.txt "build/resources/${BEAN_ARDUINO_CORE}/hardware/LightBlue-Bean/bean/avr/"
+
 echo "Build Complete!"
